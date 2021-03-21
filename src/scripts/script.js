18efe8ac87dc3e30b8fcf96172d2acd24a5bd786
Nikita <main.acr0matic@gmail.com>
2021-03-21 17:11:13 +0300

    feat: верстка "остались вопросы"

---
+++ src/scripts/script.js
@@ -0,0 +1,16 @@
+/* global tippy */
+
+// let tippyInstances = [];
+// const singleton = tippy.createSingleton([], {
+//   placement: 'left',
+//   allowHTML: true,
+//   maxWidth: 500,
+//   delay: 100,
+// });
+
+tippy('[data-tippy-content]', {
+  placement: 'right',
+  allowHTML: true,
+  maxWidth: 200,
+  theme: 'cifrodent',
+});
