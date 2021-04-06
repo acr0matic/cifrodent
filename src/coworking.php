a1bdd262bfb2386f6ff57d7d386402dea1d3ba14
Nikita <main.acr0matic@gmail.com>
2021-04-06 16:27:31 +0300

    feat: верстка главного экрана страницы "коворкинг"

---
+++ src/coworking.php
@@ -0,0 +1,78 @@
+<?php include_once('partials/header.php') ?>
+
+<main>
+  <section id="coworking">
+    <div class="container">
+      <div class="row">
+        <div class="col-11 offset-1">
+          <div class="coworking">
+            <h1 class="coworking__title page__title">
+              <span class="page__title page__title--large">Коворкинг</span>
+              <br>
+              для зубных техников
+            </h1>
+
+            <p class="coworking__description page__description">
+              Работай на современном оборудовании,
+              <br>
+              обменивайся опытом и совершенствуй
+              <br>
+              свои навыки
+            </p>
+
+            <div class="coworking__row">
+              <button class="coworking__button button button-primary">Арендовать место</button>
+
+              <div class="coworking__advantages coworking-advantages">
+                <div class="coworking-advantages__wrapper">
+                  <div class="coworking-advantages__item">
+                    <h3 class="coworking-advantages__title">24 часа</h3>
+                    <p class="coworking-advantages__description">
+                      Доступ к коворкингу
+                    </p>
+                  </div>
+                  <!-- /.coworking-advantages__item -->
+
+                  <div class="coworking-advantages__item">
+                    <h3 class="coworking-advantages__title">8 000 руб/мес</h3>
+                    <p class="coworking-advantages__description">
+                      Доступная цена с удобным расположением
+                    </p>
+                  </div>
+                  <!-- /.coworking-advantages__item -->
+
+
+                  <div class="coworking-advantages__item">
+                    <h3 class="coworking-advantages__title">м. Название</h3>
+                    <p class="coworking-advantages__description">
+                      5 минут от станции метро "Название".
+                    </p>
+                  </div>
+                  <!-- /.coworking-advantages__item -->
+
+                  <div class="coworking-advantages__item">
+                    <h3 class="coworking-advantages__title">PS4</h3>
+                    <p class="coworking-advantages__description">
+                      Для отдыха можно всегда зарубиться партию в FIFA
+                    </p>
+                  </div>
+                  <!-- /.coworking-advantages__item -->
+                </div>
+                <!-- /.coworking-advantages__wrapper -->
+              </div>
+              <!-- /.coworking__advantages coworking-advantages -->
+            </div>
+            <!-- /.coworking__row -->
+          </div>
+          <!-- /.coworking -->
+        </div>
+        <!-- /.col-11 offset-1 -->
+      </div>
+      <!-- /.row -->
+
+    </div>
+    <!-- /.container -->
+  </section>
+</main>
+
+<?php include_once('partials/footer.php') ?>
