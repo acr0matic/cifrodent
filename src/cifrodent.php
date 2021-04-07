2972bd30dab8a00bf602d507a8efe92d489159a3
Nikita <main.acr0matic@gmail.com>
2021-04-07 15:21:08 +0300

    feat: верстка главного экрана страницы "цифродент"

---
+++ src/cifrodent.php
@@ -0,0 +1,44 @@
+<?php include_once('partials/header.php') ?>
+
+<!-- Основной контент -->
+<main>
+  <section id="cifrodent-hero">
+    <div class="container">
+      <div class="cifrodent-hero">
+        <picture class="cifrodent-hero__picture">
+          <source srcset="img/pages/cifrodent/cifrodent/1.webp" type="image/webp">
+          <img src="img/pages/cifrodent/cifrodent/1.jpg" alt="" class="cifrodent-hero__image">
+        </picture>
+
+        <div class="row justify-content-end">
+          <div class="col-7">
+            <div class="cifrodent-hero__content">
+              <h1 class="cifrodent-hero__title page__title">О зуботехнической <br> лаборатории "Цифродент"</h1>
+              <p class="cifrodent-hero__description page__description">
+                Зуботехническая лаборатория "Цифродент" более 10 лет помогает врачам-стоматологам Москвы и Московской области
+              </p>
+
+              <p class="cifrodent-hero__description page__description">
+                Лаборатория обладает собственным фрезерным центром, цехом прессования и отделом принтования. Зуботехническое производство оборудовано современным европейским оборудованием
+              </p>
+
+              <p class="cifrodent-hero__description page__description">
+                Мы готовы предоставить материалы
+                и оборудование для зуботехнических лабораторий и зубных техников
+              </p>
+            </div>
+            <!-- /.cifrodent-hero__content -->
+          </div>
+          <!-- /.col-7 -->
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.cifrodent-hero -->
+
+      <?php include_once('partials/advantages.php') ?>
+    </div>
+    <!-- /.container -->
+  </section>
+</main>
+
+<?php include_once('partials/footer.php') ?>
