567c7a96a2dfb3b807f8f129e7adef696d4645cb
Nikita <main.acr0matic@gmail.com>
2021-04-08 16:08:31 +0300

    feat: добавлены две страницы

---
+++ src/pressing.php
@@ -0,0 +1,179 @@
+<?php include_once('partials/header.php') ?>
+
+<!-- Основной контент -->
+<main>
+  <section id="workshop">
+    <div class="container">
+      <div class="workshop">
+        <div class="workshop__content">
+          <h1 class="workshop__title page__title">Цех прессования</h1>
+        </div>
+        <!-- /.workshop__content -->
+
+        <picture class="workshop__picture">
+          <source srcset="img/pages/pressing/workshop/1.webp" type="image/webp">
+          <img src="img/pages/pressing/workshop/1.jpg" alt="" class="workshop__image">
+        </picture>
+      </div>
+      <!-- /.workshop -->
+    </div>
+    <!-- /.container -->
+  </section>
+
+  <section id="price" class="section">
+    <div class="container">
+      <div class="price">
+        <h2 class="price__title section__title text-center">Действующий прайс лист</h2>
+
+        <div class="row justify-content-center">
+          <div class="col-12 col-xl-10">
+            <div class="table">
+              <div class="table__wrapper">
+                <table class="price__table table__item">
+                  <tr class="table__header">
+                    <th>Артикул</th>
+                    <th>Позиция</th>
+                    <th>Срок изготовления</th>
+                    <th>Цена</th>
+                  </tr>
+
+                  <tr>
+                    <td>MILL.STL.Titan-01</td>
+                    <td>Фрезеровка мостовидной анатомической коронки из Multilayer3D / Multilayer3D PRO винтовой фиксации, 4 и более единиц в мосту (на основании заказчика)</td>
+                    <td>2 дня</td>
+                    <td>22 850</td>
+                  </tr>
+
+                  <tr>
+                    <td>MILL.STL.Titan-01</td>
+                    <td>Фрезеровка мостовидной анатомической коронки </td>
+                    <td>2 дня</td>
+                    <td>22 850</td>
+                  </tr>
+
+                  <tr>
+                    <td>MILL.STL.Titan-01</td>
+                    <td>Фрезеровка мостовидной анатомической коронки </td>
+                    <td>2 дня</td>
+                    <td>22 850</td>
+                  </tr>
+
+                  <tr>
+                    <td>MILL.STL.Titan-01</td>
+                    <td>Фрезеровка мостовидной анатомической коронки </td>
+                    <td>2 дня</td>
+                    <td>22 850</td>
+                  </tr>
+
+                  <tr>
+                    <td>MILL.STL.Titan-01</td>
+                    <td>Фрезеровка мостовидной анатомической коронки </td>
+                    <td>2 дня</td>
+                    <td>22 850</td>
+                  </tr>
+
+                  <tr>
+                    <td>MILL.STL.Titan-01</td>
+                    <td>Фрезеровка мостовидной анатомической коронки </td>
+                    <td>2 дня</td>
+                    <td>22 850</td>
+                  </tr>
+                </table>
+              </div>
+              <!-- /.table__wrapper -->
+            </div>
+            <!-- /.table -->
+          </div>
+          <!-- /.col-10 -->
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.price -->
+    </div>
+    <!-- /.container -->
+  </section>
+
+  <section id="gallery" class="section">
+    <div class="container">
+      <h2 class="gallery__title section__title text-center">Наши работы</h2>
+
+      <div class="gallery">
+        <div class="swiper-container slider-gallery slider--bootstrap">
+          <div class="swiper-wrapper">
+            <div class="swiper-slide">
+              <a href="img/pages/pressing/gallery/1.jpg" data-fslightbox="">
+                <picture class="gallery__picture">
+                  <source srcset="img/pages/pressing/gallery/1.webp" type="image/webp">
+                  <img class="gallery__image" src="img/pages/pressing/gallery/1.jpg" alt="">
+                </picture>
+              </a>
+            </div>
+            <!-- /.swiper-slide -->
+
+            <div class="swiper-slide">
+              <a href="img/pages/pressing/gallery/2.jpg" data-fslightbox="">
+                <picture class="gallery__picture">
+                  <source srcset="img/pages/pressing/gallery/2.webp" type="image/webp">
+                  <img class="gallery__image" src="img/pages/pressing/gallery/2.jpg" alt="">
+                </picture>
+              </a>
+            </div>
+            <!-- /.swiper-slide -->
+
+            <div class="swiper-slide">
+              <a href="img/pages/pressing/gallery/3.jpg" data-fslightbox="">
+                <picture class="gallery__picture">
+                  <source srcset="img/pages/pressing/gallery/3.webp" type="image/webp">
+                  <img class="gallery__image" src="img/pages/pressing/gallery/3.jpg" alt="">
+                </picture>
+              </a>
+            </div>
+            <!-- /.swiper-slide -->
+
+            <div class="swiper-slide">
+              <a href="img/pages/pressing/gallery/4.jpg" data-fslightbox="">
+                <picture class="gallery__picture">
+                  <source srcset="img/pages/pressing/gallery/4.webp" type="image/webp">
+                  <img class="gallery__image" src="img/pages/pressing/gallery/4.jpg" alt="">
+                </picture>
+              </a>
+            </div>
+            <!-- /.swiper-slide -->
+
+            <div class="swiper-slide">
+              <a href="img/pages/pressing/gallery/5.jpg" data-fslightbox="">
+                <picture class="gallery__picture">
+                  <source srcset="img/pages/pressing/gallery/5.webp" type="image/webp">
+                  <img class="gallery__image" src="img/pages/pressing/gallery/5.jpg" alt="">
+                </picture>
+              </a>
+            </div>
+            <!-- /.swiper-slide -->
+
+            <div class="swiper-slide">
+              <a href="img/pages/pressing/gallery/6.jpg" data-fslightbox="">
+                <picture class="gallery__picture">
+                  <source srcset="img/pages/pressing/gallery/6.webp" type="image/webp">
+                  <img class="gallery__image" src="img/pages/pressing/gallery/6.jpg" alt="">
+                </picture>
+              </a>
+            </div>
+            <!-- /.swiper-slide -->
+          </div>
+          <!-- /.swiper-wrapper -->
+
+          <div class="swiper-controls swiper-controls--mobile">
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+          </div>
+          <!-- /.swiper-controls -->
+        </div>
+        <!-- /.swiper-container -->
+      </div>
+      <!-- /.gallery -->
+    </div>
+    <!-- /.container -->
+  </section>
+</main>
+
+<?php include_once('partials/footer.php') ?>
