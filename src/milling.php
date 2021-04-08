f13576825243dc377cea0fab4706ce2d824916df
Nikita <main.acr0matic@gmail.com>
2021-04-08 15:41:56 +0300

    feat: верстка таблицы

---
+++ src/milling.php
@@ -0,0 +1,74 @@
+<?php include_once('partials/header.php') ?>
+
+<!-- Основной контент -->
+<main>
+  <section id="workshop">
+    <div class="container">
+      <div class="workshop">
+        <div class="workshop__content">
+          <h1 class="workshop__title page__title">Фрезерный центр</h1>
+        </div>
+        <!-- /.workshop__content -->
+
+        <picture class="workshop__picture">
+          <source srcset="img/pages/milling/workshop/1.webp" type="image/webp">
+          <img src="img/pages/milling/workshop/1.jpg" alt="" class="workshop__image">
+        </picture>
+      </div>
+      <!-- /.workshop -->
+    </div>
+    <!-- /.container -->
+  </section>
+
+  <section id="price" class="section">
+    <div class="container">
+      <div class="price">
+        <div class="row justify-content-center">
+          <div class="col-12 col-xl-10">
+            <div class="table">
+              <div class="table__wrapper">
+                <table class="price__table table__item">
+                  <tr class="table__header">
+                    <th>Артикул</th>
+                    <th>Позиция</th>
+                    <th>Срок изготовления</th>
+                    <th>Цена</th>
+                  </tr>
+
+                  <tr>
+                    <td>MILL.STL.Titan-01</td>
+                    <td>Фрезеровка мостовидной анатомической коронки из Multilayer3D / Multilayer3D PRO винтовой фиксации, 4 и более единиц в мосту (на основании заказчика)</td>
+                    <td>2 дня</td>
+                    <td>22 850</td>
+                  </tr>
+
+                  <tr>
+                    <td>MILL.STL.Titan-01</td>
+                    <td>Фрезеровка мостовидной анатомической коронки </td>
+                    <td>2 дня</td>
+                    <td>22 850</td>
+                  </tr>
+
+                  <tr>
+                    <td>MILL.STL.Titan-01</td>
+                    <td>Фрезеровка мостовидной анатомической коронки </td>
+                    <td>2 дня</td>
+                    <td>22 850</td>
+                  </tr>
+                </table>
+              </div>
+              <!-- /.table__wrapper -->
+            </div>
+            <!-- /.table -->
+          </div>
+          <!-- /.col-10 -->
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.price -->
+    </div>
+    <!-- /.container -->
+  </section>
+</main>
+
+<?php include_once('partials/footer.php') ?>
